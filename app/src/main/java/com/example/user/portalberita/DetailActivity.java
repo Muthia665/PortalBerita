ced64c74648c3bc0bc544ec3a986d4063bc76f3f
muthia665 <muthia09876@gmail.com>
2021-07-19 23:22:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/user/portalberita/DetailActivity.java
@@ -0,0 +1,81 @@
+package com.example.user.portalberita;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.androidquery.AQuery;
+import com.androidquery.callback.AjaxCallback;
+import com.androidquery.callback.AjaxStatus;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class DetailActivity extends AppCompatActivity {
+
+    TextView txtjudul, txtisi;
+    ImageView img;
+    AQuery aQuery;
+    String id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Intent a = getIntent();
+        id = a.getStringExtra("id");
+        getDetailBerita();
+    }
+
+    private void getDetailBerita() {
+        String url = "http://192.168.100.4/portal_berita/detailberita.php";
+        HashMap<String, String> hm = new HashMap<>();
+        hm.put("id_berita", id);
+        ProgressDialog progressDialog = new ProgressDialog(DetailActivity.this);
+        progressDialog.setMessage("Loading .   .   .");
+        aQuery = new AQuery(DetailActivity.this);
+        aQuery.progress(progressDialog).ajax(url, hm, String.class, new AjaxCallback<String>(){
+            @Override
+            public void callback(String url, String object, AjaxStatus status) {
+                if (object != null) {
+                    try {
+                        JSONObject json = new JSONObject(object);
+                        String result = json.getString("pesan");
+                        String sukses = json.getString("sukses");
+
+                        if (sukses.equalsIgnoreCase("true")){
+                            JSONArray jsonArray = json.getJSONArray("berita");
+                            JSONObject jsonObject = jsonArray.getJSONObject(0);
+                            String judul = jsonObject.getString("judul");
+                            String gambar = jsonObject.getString("gambar");
+                            String isi = jsonObject.getString("isi");
+                            ActionGet(judul, gambar, isi);
+
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    private void ActionGet(String judul, String gambar, String isi) {
+        txtjudul = findViewById(R.id.txtdetailjudul);
+        txtisi = findViewById(R.id.txtdetailisi);
+        img = findViewById(R.id.imgdetail);
+
+        txtjudul.setText(judul);
+        txtisi.setText(isi);
+        String urlfoto = "http://192.168.100.4/portal_berita/foto_berita/";
+        Picasso.with(DetailActivity.this).load(urlfoto + gambar).error(R.drawable.ss).into(img);
+    }
+}
